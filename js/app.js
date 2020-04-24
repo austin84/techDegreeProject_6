11c6dc734b1fc26b19ef2ef06de1cb34dc78371b
austin84 <austinveatch84@gmail.com>
2020-04-24 10:13:57 -0700

    create array of phrases

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
+// ** Variables
+
 const keyboard = document.getElementById("qwerty");
 const phraseArea = document.getElementById("phrase");
 const button = document.querySelector(".btn_reset");
+let missed = 0;
+
+// ** Functions
+
+// ** Array of Phrases
+
+const phrases = [
+  "IT WILL GROW ON YOU",
+  "PLEASE FASTEN YOUR SEAT BELTS",
+  "OUR DOOR IS ALWAYS OPEN",
+  "IN THE RIGHT FRAME OF MIND",
+  "ALL THAT JAZZ",
+  "PUT ON YOUR THINKING CAP",
+  "THE ROOT OF THE MATTER",
+  "ENGROSSED IN THOUGHT",
+  "FINISH YOUR THOUGHT",
+  "TOP AGRICULTURAL PRODUCER IN AMERICA",
+  "STREETS AHEAD",
+  "VARIETY IS THE SPICE OF LIFE",
+];
