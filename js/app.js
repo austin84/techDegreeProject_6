8a58292370a6a63eeb39453aba5a48f3c90b5476
austin84 <austinveatch84@gmail.com>
2020-04-24 11:22:34 -0700

    start button enabled

--- js/app.js
+++ js/app.js
@@ -3,23 +3,38 @@
 const keyboard = document.getElementById("qwerty");
 const phraseArea = document.getElementById("phrase");
 const button = document.querySelector(".btn_reset");
+const overlay = document.getElementById("overlay");
 let missed = 0;
-
-// ** Functions
 
 // ** Array of Phrases
 
 const phrases = [
-  "IT WILL GROW ON YOU",
-  "PLEASE FASTEN YOUR SEAT BELTS",
-  "OUR DOOR IS ALWAYS OPEN",
-  "IN THE RIGHT FRAME OF MIND",
-  "ALL THAT JAZZ",
-  "PUT ON YOUR THINKING CAP",
-  "THE ROOT OF THE MATTER",
-  "ENGROSSED IN THOUGHT",
-  "FINISH YOUR THOUGHT",
-  "TOP AGRICULTURAL PRODUCER IN AMERICA",
-  "STREETS AHEAD",
-  "VARIETY IS THE SPICE OF LIFE",
+  "it will grow on you",
+  "please fasten your seat belts",
+  "our door is always open",
+  "in the right frame of mind",
+  "all that jazz",
+  "put on your thinking cap",
+  "the root of the matter",
+  "engrossed in thought",
+  "finish your thought",
+  "top agricultural producer in america",
+  "streets ahead",
+  "variety is the spice of life",
 ];
+
+// ** Functions
+
+// starts event listener for 'start game' button
+const startGame = () => {
+  button.addEventListener("click", (event) => {
+    if (overlay.style.display !== "none") {
+      overlay.style.display = "none";
+    }
+  });
+
+  // ** Script
+
+  //game begins, start game button enabled
+  startGame();
+};
